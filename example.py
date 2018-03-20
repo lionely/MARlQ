b72899d5f5cd126c9e0b5e8f3e10bd744fed297e
lionely <lionelscotty95@gmail.com>
2018-03-20 14:08:31 -0500

    Modified the loading and saving functions to load files from the folder Q-tables.

--- example.py
+++ example.py
@@ -8,12 +8,13 @@ Created on Fri Feb 16 11:26:15 2018
 
 import gym
 #import numpy 
-#import pickle_utilities
+import pickle_utilities as pu
 from ql_box import *
 #import itertools
 #import random
 #import wrappers 
 
+#TODO Figure out how to stop mario from getting stuck.
 def playAsHuman(env, playTime=1000):
     #TODO: make this work...!
     #TODO: make this run based on time and kill it after the time is over.
@@ -24,60 +25,13 @@ def playAsHuman(env, playTime=1000):
 
 #Should hold down jump, to be able to jump higher."
 
-#TODO: Added docustring but this function is not complete yet, will do after we clear level 1.
-"""This function takes an environment and Q table and checks if the optimal actions
-at each state is actually being taken. """
-def test_algorithm(env,Q):
-    stuck_capacity = 5
-    stuck = []
 
-    observation = env.reset()
-    total_reward = 0
-    action = [0]*6
-    observation,reward,done,info = env.step(action)
-    state = info['distance']
-    action_dict = {'up':    [1, 0, 0 ,0, 0, 0],
-                   'L':     [0, 1, 0, 0, 0, 0],
-                   'down':  [0, 0, 1, 0, 0, 0],
-                   'R':     [0, 0, 0, 1, 0, 0],
-                   'JUMP':  [0, 0, 0, 0, 1, 0],
-                   'B':     [0, 0, 0, 0, 0, 1]}
-    for t in itertools.count():
-        # selection the action with highest values i.e. best action
-        max_q_action = max(Q[state], key=lambda key: Q[state][key])
-#        if len(stuck)<stuck_capacity:
-#            stuck.append(max_q_action)
-#        stuck = [] if t%10==0 else stuck
-        #print('t is ',t )
-#        if isStuck(stuck,stuck_capacity):
-#            print('is stuck :(')
-#            max_q_action = 'R' #random.choice(Q[state].keys())
-#            stuck = []
-        print("Optimal action is: " + max_q_action)
-        action = action_dict[str(max_q_action)]
-        print("Action is: " , action)
-        # apply selected action
-        observation, reward, done,info = env.step(action)
-        print("Reward: ", reward)
-        next_state = info['distance']
-        # calculate total reward
-        total_reward += reward
-        if done:
-            print(total_reward)
-            break
-        state = next_state
-    return total_reward
-"""Possibly a helper function to test_algorithm"""
-def isStuck(stuck,capacity):
-    if len(stuck) == capacity:
-        stuck = []
-    return len(np.unique(stuck)) == 1
-
+#TODO collect total reward after every 5 episodes, max distance, episodes ran so far
 if __name__ == "__main__":
     env = gym.make('SuperMarioBros-1-1-Tiles-v0')  # remember need to make the environment each time
-    Q = ql_box(env, 1)
-    box_Q = loadLatestWith('ql_box')
-    
-    #Q = ql_distScore(env, 10)
-
+    #Q = pu.loadQ('ql_box_245_2.pickle')
+    #test_algorithm(env)
+    for i in range(2):
+        print(str(i*5) + ' episodes have been ran.')
+        Q = ql_box(env, 5)
     
--- pickle_utilities.py
+++ pickle_utilities.py
@@ -10,10 +10,11 @@ import pickle
 import glob
 import sys
 import os
+import pandas as pd
 
 #TODO Is there a better way to search for extensions with pickle?
 def hasPickleWith(functionName):
-    database = filter(os.path.isfile, glob.glob('./*.pickle'))
+    database = filter(os.path.isfile, glob.glob('Q-tables/*.pickle'))
     if database:
         for file in database:
             if functionName in file:
@@ -27,10 +28,10 @@ def saveQ(Q, num_episodes, functionName,boxSize=""):
     # For example, if initially done 10 episodes, num_episodes==10.
     # If do 20 more episodes, num_episodes==30.
     if functionName == 'q_learning':
-        with open(functionName + '_' +str(num_episodes)+'.pickle', 'wb') as handle:
+        with open('Q-tables/'+functionName + '_' +str(num_episodes)+'.pickle', 'wb') as handle:
             pickle.dump(Q, handle, protocol=pickle.HIGHEST_PROTOCOL)
     else:
-        with open(functionName + '_'+str(num_episodes)+'_'+str(boxSize)+'.pickle', 'wb') as handle:
+        with open('Q-tables/'+functionName + '_'+str(num_episodes)+'_'+str(boxSize)+'.pickle', 'wb') as handle:
             pickle.dump(Q, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print("Saved Q table succesfully for "+str(num_episodes)+" episodes!")
     return
@@ -45,29 +46,29 @@ def loadLatestWith(functionName):
     episode_stamps = []
     f_name = ''
     # for file in _glob.glob("*.pickle"):   #PYTHON2
-    for file in glob.glob("*.pickle"):      #PYTHON3    https://stackoverflow.com/questions/44366614/nameerror-name-glob-is-not-defined
+    for file in glob.glob("Q-tables/*.pickle"):      #PYTHON3    https://stackoverflow.com/questions/44366614/nameerror-name-glob-is-not-defined
         if functionName in file and functionName=='q_learning':
-            f_name = str(file)
+            f_name = str(file).replace("Q-tables/","",1)
             #Most recent is defined as the one that makes it the furtherest distance.
             f_name_offset = len(functionName)-1 # to get the index of where it starts
             e_stamp = f_name[f_name_offset+2:]
             end_ = e_stamp.index('_')
-            episode_stamps.append(e_stamp[:end_])
+            episode_stamps.append(int (e_stamp[:end_]) )
         elif functionName in file:
-            f_name = str(file)
+            f_name = str(file).replace("Q-tables/","",1)
             box_num = f_name[-1]
             #Most recent is defined as the one that makes it the furtherest distance.
             f_name_offset = len(functionName)-1 # to get the index of where it starts
             e_stamp = f_name[f_name_offset+2:]
             end_ = e_stamp.index('_')
-            episode_stamps.append(e_stamp[:end_])
-
-    max_e_stamp = max(episode_stamps)
+            episode_stamps.append(int (e_stamp[:end_]) )
+    #print("The stamps are: ",episode_stamps)
+    max_e_stamp = str(max(episode_stamps))
 
     #Why are we looping through twice??
     #I looped to find the latest pickle then after we found That
     # we load. There might be a way to load based on the most recent episode. We can look into it!
-    for file in glob.glob("*.pickle"):
+    for file in glob.glob("Q-tables/*.pickle"):
         if functionName in file and functionName=='q_learning':
             f_name = str(file)
             if max_e_stamp in f_name:
@@ -78,3 +79,26 @@ def loadLatestWith(functionName):
                 break
     print("Loaded: " + f_name)
     return  (loadQ(f_name) , int(max_e_stamp) )
+
+"""Saves a csv with Pandas"""
+def collectData(episode_num,reward,dist,functionName):
+    log = {'episode_num': [episode_num] ,'reward': [reward], 'dist': [dist]}
+    stats_df = pd.DataFrame(data=log)
+    filename = 'reward_stats/'+functionName+'_reward_stats.csv'
+    if not os.path.isfile(filename):
+        stats_df.to_csv(filename, sep=',', encoding='utf-8',index=False)
+    stats = pd.read_csv(filename)
+    stats = stats.append(stats_df,ignore_index=True)
+    stats.to_csv(filename, sep=',', encoding='utf-8',index=False)
+
+    print("Saved Episode stats succesfully for "+str(episode_num)+" episodes!")
+    return
+#log = {'episode_num': [100] ,'reward': [1000], 'dist': [1000]}
+#filename = 'reward_stats/'+'ql_box'+'_reward_stats.csv'
+#stats_df = pd.DataFrame(data=log)
+#if not os.path.isfile(filename):
+#       stats_df.to_csv(filename, sep=',', encoding='utf-8',index=False)
+#
+#stats = pd.read_csv(filename)
+#stats = stats.append(stats_df,ignore_index=True)
+#stats.to_csv(filename, sep=',', encoding='utf-8',index=False)
